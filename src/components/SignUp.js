408421adceeeaf9f81890831d9e29f99dc3f0c1d
Zahra Khan <zahrakhan@Zahras-Air.fios-router.home>
2022-11-01 12:32:06 -0400

    added new files and used components for react

---
+++ src/components/SignUp.js
@@ -0,0 +1,45 @@
+
+
+import "../style/SignUp.css"
+import {useState} from 'react'
+//using props to connect one data to another
+//when you sign up successfully, you can see the welcome page. This is happening because of states and props.
+const SignUp = (props) => {
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    //this tied to a dunction like changePassword function
+    const changePassword = (event) => {
+        setPassword(event.target.value)
+    }
+    const changeConfirmPassword = (event) => {
+        setConfirmPassword(event.target.value)
+    }
+    
+    const validatePasswords = () => {
+       //event.preventDefault()
+        if(password === confirmPassword){
+            props.userAuthChanged()
+            //alert("It worked!")
+        }else{
+            alert("Your passwords are not the same. Try again")
+        }
+    }
+
+
+    return(
+    <div>
+            <h1>Sign Up!</h1>
+     
+        <form onSubmit={validatePasswords} className="sign-up">
+           
+            <input type = "text" />
+            <input type = "password" onChange={changePassword} value={password}/>
+            <input type = "password" onChange={changeConfirmPassword} value={confirmPassword}/>
+            <input type = "submit" />
+        </form>
+    </div>
+    )
+
+}
+export default SignUp
